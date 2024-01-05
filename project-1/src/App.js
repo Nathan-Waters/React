7cb99bd5169b262a4faea45a4e8ba3d70bf6a2d7
NathanWaters <nathanwaters94@gmail.com>
2024-01-05 09:19:22 -0800

    adding additional things

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 
-
+//adding something
 function App() {
   return (
     <div className="container">
